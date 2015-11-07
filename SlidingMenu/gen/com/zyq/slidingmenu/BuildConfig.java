9db3ce96c2a3f1856efbbe0f514e23dbfd81227a
zyq1903 <1741815869@qq.com>
2015-11-07 13:53:03 +0800

    删除多余的文档

--- SlidingMenu/gen/com/zyq/slidingmenu/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.zyq.slidingmenu;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- SortListViewDemo/gen/com/dy/ustc/sortlistviewdemo/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.dy.ustc.sortlistviewdemo;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
